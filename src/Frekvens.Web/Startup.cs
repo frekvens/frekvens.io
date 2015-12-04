008b4eeaf7cbf332cb19a4b4b569589868164538
ellern <martin@frekvens.io>
2015-12-04 12:11:24 +0100

    Updated keys for lets encrypt

--- src/Frekvens.Web/Startup.cs
+++ src/Frekvens.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Collections.Generic;
 
 [assembly: OwinStartup(typeof(Frekvens.Web.Startup))]
 
@@ -9,9 +10,24 @@ namespace Frekvens.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseLetsEncrypt("4fMwlcYNvVsTQ5b5bxL2DUx4SWM4b21QmYxvfO6c7EM", "4fMwlcYNvVsTQ5b5bxL2DUx4SWM4b21QmYxvfO6c7EM.52CEr9yUu-5FmKsp7VokFUHBBhsJxay5egIhVaYCrv4");
+            app.UseLetsEncrypt("yBgFcsYafEd2czuxWXHg-8sLQ7FNM8sN1j_4XHcK0NM", "yBgFcsYafEd2czuxWXHg-8sLQ7FNM8sN1j_4XHcK0NM.52CEr9yUu-5FmKsp7VokFUHBBhsJxay5egIhVaYCrv4");
             app.UseLetsEncrypt("EL9feWrgVi4BczT9cT707HiJc_s0jp6xwH9XUejLN3Y", "EL9feWrgVi4BczT9cT707HiJc_s0jp6xwH9XUejLN3Y.52CEr9yUu-5FmKsp7VokFUHBBhsJxay5egIhVaYCrv4");
         }
+    }
+
+    public class LetsEncryptOptions
+    {
+        public LetsEncryptOptions(string accountEmail, string accountPublicKey)
+        {
+            AccountEmail = accountEmail;
+            AccountPublicKey = accountPublicKey;
+        }
+
+        public string AccountEmail { get; protected set; }
+        public string AccountPublicKey { get; protected set; }
+        public string AccountPrivateKey { get; set; }
+        //public string CertificateSigningRequest { get; set; }
+        //public List<string> Domains { get; set; }
     }
 
     public static class LetsEncryptExtensions
